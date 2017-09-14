7ad7c564589ac2e2c49dfa003a719b05f96ee3ad
darrensoap <darrensoap@gmail.com>
2017-09-14 16:53:42 +0800

    backend withdraw function completed

---
+++ backend/modules/finance/views/withdraw/index.php
@@ -0,0 +1,111 @@
+<?php
+
+/* @var $this yii\web\View */
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\grid\GridView;
+use yii\grid\ActionColumn;
+use yii\db\ActiveRecord;
+use iutbay\yii2fontawesome\FontAwesome as FA;
+use kartik\widgets\ActiveForm;
+
+	$this->title = 'User Withdraw';
+	$this->params['breadcrumbs'][] = $this->title;
+	
+?>
+	<?= GridView::widget([
+        'dataProvider' => $model,
+        'filterModel' => $searchModel,
+        'showFooter'=>true,
+
+        'columns' => [
+	        ['class' => 'yii\grid\ActionColumn',
+				'template'=>'{approve} ',
+	            'header' => "Approve",
+				'buttons' => [
+	            'approve' => function($url , $model){
+						
+	                    $url = Url::to(['withdraw/approve' ,'id'=>$model->id,'admin'=>Yii::$app->user->identity->id]) ;
+	                    return Html::a('Approve' , $url , ['title' => 'Approve','data-confirm'=>"Confirm action?"]);
+						
+					},
+	            ]
+			],
+			
+			['class' => 'yii\grid\ActionColumn' , 
+	            'template'=>'{cancel} ',
+	            'header' => "Reject",
+				'buttons' => [
+	            'cancel' => function($url , $model){
+						
+	                	$url = Url::to(['withdraw/cancel' ,'id'=>$model->id]) ;
+	                    return Html::a('Cancel' , $url , ['title' => 'Reject','data-confirm'=>"Confirm action?"]);
+						
+					},
+	            ]
+			],
+
+			[
+                'attribute' => 'acc_name',
+                'filterInputOptions' => [
+                    'class'       => 'form-control',
+                    'placeholder' => 'Search Account Name',
+                ],
+            ],
+
+            [
+                'attribute' => 'withdraw_amount',
+                'filterInputOptions' => [
+                    'class'       => 'form-control',
+                    'placeholder' => 'Search Amount',
+                ],
+            ],
+
+            [
+                'attribute' => 'to_bank',
+                'filterInputOptions' => [
+                    'class'       => 'form-control',
+                    'placeholder' => 'Search Bank Acount',
+                ],
+            ],
+
+            [
+                'attribute' => 'bank_name',
+                'filterInputOptions' => [
+                    'class'       => 'form-control',
+                    'placeholder' => 'Search Bank Name',
+                ],
+            ],
+
+			[
+				'label' => 'Status',
+				'format' => 'raw',
+				'headerOptions' => ['width' => "15px"],
+				'contentOptions' => ['style' => 'font-size:20px;'],
+				'attribute' => 'accounttopup_status.title',
+				'value' => function($model){
+					return Html::tag('span' , $model->accounttopup_status->title);
+				},
+
+				'filter' => $list,
+			],
+			
+			[
+                'attribute' => 'inCharge',
+                'filterInputOptions' => [
+                    'class'       => 'form-control',
+                    'placeholder' => 'Search In Charge Person',
+                ],
+            ],
+
+            [
+                'attribute' => 'reason',
+                'filterInputOptions' => [
+                    'class'       => 'form-control',
+                    'placeholder' => 'Search Reason',
+                ],
+            ],
+		],
+               
+    	         
+    ]); ?>
--- common/models/Withdraw.php
+++ common/models/Withdraw.php
@@ -6,6 +6,7 @@ use Yii;
 use yii\behaviors\TimestampBehavior;
 use yii\db\ActiveRecord;
 use yii\data\ActiveDataProvider;
+use common\models\Accounttopupstatus;
 
 /**
  * This is the model class for table "withdraw".
@@ -80,5 +81,41 @@ class Withdraw extends \yii\db\ActiveRecord
             'created_at' => 'Created At',
             'updated_at' => 'Updated At',
         ];
+    }
+
+    public function attributes()
+    {
+        return array_merge(parent::attributes(),['accounttopup_status.id','accounttopup_status.title']);
+    }
+    
+    public function getAccounttopup_status()
+    {
+        return $this->hasOne(Accounttopupstatus::className(),['id' => 'action']); 
+    }
+
+    public function search($params,$action)
+    {
+        if ($action == 0){
+            $query = self::find();
+        }
+        elseif ($action >= 1){
+            $query = self::find()->where('action = :act',[':act' => $action]);
+        }
+
+        $query->joinWith(['accounttopup_status']);
+        $dataProvider = new ActiveDataProvider(['query' => $query,
+        ]);
+        $this->load($params);
+        $query->andFilterWhere([
+            'title' => $this->getAttribute('accounttopup_status.title'),
+        ]);
+
+        $query->andFilterWhere(['like','acc_name' ,  $this->acc_name])
+                ->andFilterWhere(['like','withdraw_amount' ,  $this->withdraw_amount])
+                ->andFilterWhere(['like','to_bank' ,  $this->to_bank])
+                ->andFilterWhere(['like','bank_name' ,  $this->bank_name])
+                ->andFilterWhere(['like','inCharge' ,  $this->inCharge])
+                ->andFilterWhere(['like','reason' ,  $this->reason]);
+        return $dataProvider;
     }
 }

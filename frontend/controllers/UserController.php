6f919b8a9489b47fda37e1d3085fc42874964f1b
shijuin97 <ngshijuin@gmail.com>
2017-09-19 13:05:33 +0800

    vxcv

--- frontend/controllers/UserController.php
+++ frontend/controllers/UserController.php
@@ -14,6 +14,7 @@ class UserController extends Controller
     public function actionUserProfile()
     {
         $user = User::find()->where('id = :id' ,[':id' => Yii::$app->user->id])->joinWith(['useraddress','userdetails'])->one();
+        $this->layout = 'user';
         
         return $this->render('userprofile',['user' => $user]);
        

22a1cfbc28db0c77e8cdfa925e674a5a30564781
ding94 <dw-ding@hotmail.co.uk>
2017-09-27 10:32:10 +0800

    backend sign in done

--- backend/views/deliveryman/daily.php
+++ backend/views/deliveryman/daily.php
@@ -4,14 +4,33 @@
 use yii\helpers\Html;
 use yii\helpers\Url;
 use yii\grid\GridView;
-use yii\grid\ActionColumn;
-use yii\db\ActiveRecord;
-use iutbay\yii2fontawesome\FontAwesome as FA;
 use yii\bootstrap\Modal;
+use kartik\date\DatePicker;
 
 	$this->title = 'Delivery Man Daily Status';
 	$this->params['breadcrumbs'][] = $this->title;
 ?>
+<div class="row">
+	<div class="col-md-2">
+		<?php
+			echo DatePicker::widget([
+		    'name' => 'chose Date',
+		     'removeButton' => false,
+		    'pluginOptions' => [
+		        'autoclose'=>true,
+		        'format' => 'yyyy-mm-d'
+		    ],
+		    'pluginEvents' => [
+		    	'changeDate' => 'function (e){
+	                var date = $("#w0").val();
+	                var arr = date.split("-");
+	               	location.href = "/index.php?r=deliveryman/daily-signin&month="+arr[0]+"-"+arr[1]+"&day="+arr[2];
+	             }',
+		    ],
+		]);
+		?>
+	</div>
+</div>
 
 <?= GridView::widget([
         'dataProvider' => $model,

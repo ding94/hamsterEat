8943e142e5cee355641ca9e3ddc36ee3ebe4397f
lene1994 <lene.ng94@gmail.com>
2017-09-27 17:47:53 +0800

    frontend photo edited INCOMPLETE

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -11,10 +11,10 @@ $this->title = 'My Yii Application';
 <div class="site-index">
 
     
-        <div id="SlideShowContainer">
+        <div >
 
         <div class="mySlides">
-        <img src="SysImg/Img1-1200x400.jpg" style="width:96vw; height:100vh; !important;">
+        <img src="SysImg/1.jpg" >
         </div>
     
          </div>

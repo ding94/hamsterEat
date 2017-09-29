53f248587fe1c8a8351be808c198b3dc968a66f0
iShinYxR <31298177+iShinYxR@users.noreply.github.com>
2017-09-29 13:59:38 +0800

    Server Test

--- frontend/views/order/myorders.php
+++ frontend/views/order/myorders.php
@@ -1,5 +1,6 @@
 <?php
 /* @var $this yii\web\View */
+use yii\helpers\Html;
 $this->title = "My Orders";
 ?>
 <div class = "container">
@@ -12,6 +13,7 @@ $this->title = "My Orders";
                 echo "<th><center> Delivery ID </th>";
                 echo "<th><center> Current Status </th>";
                 echo "<th><center> Date and Time Placed </th>";
+                echo "<th><center> Rate </th>";
             echo "</tr>";
         foreach ($orders as $orders) : 
 
@@ -21,8 +23,16 @@ $this->title = "My Orders";
                     echo "<td><center><a href=".yii\helpers\Url::to(['order-details','did'=>$orders['Delivery_ID']]).">".$orders['Orders_Status']."</a></td>";
                     date_default_timezone_set("Asia/Kuala_Lumpur");
                     echo "<td><center><a href=".yii\helpers\Url::to(['order-details','did'=>$orders['Delivery_ID']]).">".date('d/m/Y H:i:s', $orders['Orders_DateTimeMade'])."</a></td>";
+                    if ($orders['Orders_Status']!= 'Completed')
+                    {
+                        echo "</tr>";
+                    }
+                    else
+                    {
+                    echo "<td><center>".Html::a('Rate This Delivery', ['rating/index','id'=>$orders['Delivery_ID']], ['class'=>'btn btn-primary'])."</td>";
                     //echo "</a>";
                 echo "</tr>";
+                    }
             endforeach;
         echo "</table>"; ?>
     </div>

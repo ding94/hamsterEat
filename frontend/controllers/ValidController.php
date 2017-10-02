851929186c06ba93a6c0ec5299ae0c9fa9d1b924
soonws <soonws97@gmail.com>
2017-10-02 11:39:37 +0800

    coupon date validate first done

---
+++ frontend/controllers/ValidController.php
@@ -0,0 +1,35 @@
+<?php
+namespace frontend\controllers;
+
+use Yii;
+use yii\web\Controller;
+use yii\filters\VerbFilter;
+use yii\helpers\ArrayHelper;
+use yii\filters\AccessControl;
+use common\models\Vouchers;
+
+
+class ValidController extends Controller
+{
+	public static function dateValidCheck($post,$case)
+	{
+		$voucher = Vouchers::find()->where('code = :c',[':c' => $post])->one();
+		
+		switch ($case) {
+			case 1:
+				if ($voucher->endDate <= strtotime(date('Y-m-d h:i:s'))) {
+					Yii::$app->session->setFlash('error','Coupon was expired!');
+					return false;
+				}
+				elseif ($voucher->endDate >= strtotime(date('Y-m-d h:i:s'))) {
+					return true;
+				}
+				break;
+			
+			default:
+				Yii::$app->session->setFlash('error','Something went wrong');
+					return false;
+				break;
+		}
+	}
+}

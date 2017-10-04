142665206b3b6c22d4f19d56cf80d0b3cce4bd6d
darrensoap <darrensoap@gmail.com>
2017-10-04 11:56:09 +0800

    about frontend design

--- frontend/views/site/about.php
+++ frontend/views/site/about.php
@@ -20,21 +20,21 @@ $this->title = 'About';
 
             <div class="col-lg-4 element1" style="text-align:center; position: relative;">
 	 <div class="img-circular" style="position: absolute; z-index: 10"></div>
-	  <div class="content-box" style="position: absolute; width: 200px; height: 100px; border:1.5px solid crimson; top: 100px; left: 100px;">
+	  <div class="content-box" style="position: absolute; width: 150px; height: 200px; border:1.5px solid crimson; top: 100px; left: 100px; -webkit-border-radius: 15px; -moz-border-radius: 15px; border-radius: 15px;">
       <h4 class="service-heading">Customer</h4>
       <p>testing content 123</p>
       </div>  
 	 </div>
 	  <div class="col-lg-4 element2" style="text-align:center">
 	 <div class="img-circular" style="position: absolute; z-index: 10"></div>
-	   <div class="content-box" style="position: absolute; width: 200px; height: 100px; border:1.5px solid green; top: 100px; left: 100px;">
+	   <div class="content-box" style="position: absolute; width: 150px; height: 200px; border:1.5px solid green; top: 100px; left: 100px; -webkit-border-radius: 15px; -moz-border-radius: 15px; border-radius: 15px;">
        <h4 class="service-heading">Customer</h4>
        <p>testing content 123</p>
        </div>  
 	 </div>
 	  <div class="col-lg-4 element3" style="text-align:center">
 	 <div class="img-circular" style="position: absolute; z-index: 10"></div>
-	   <div class="content-box" style="position: absolute; width: 200px; height: 100px; border:1.5px solid yellow; top: 100px; left: 100px;">
+	   <div class="content-box" style="position: absolute; width: 150px; height: 200px; border:1.5px solid yellow; top: 100px; left: 100px; -webkit-border-radius: 15px; -moz-border-radius: 15px; border-radius: 15px;">
        <h4 class="service-heading">Customer</h4>
        <p>testing content 123</p>
        </div>  

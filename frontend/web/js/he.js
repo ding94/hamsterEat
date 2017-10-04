a89aeaef3f793ed60de294805baad7241e26d3f3
darrensoap <darrensoap@gmail.com>
2017-10-04 09:58:40 +0800

    jquery animation infinite loop

--- frontend/web/js/he.js
+++ frontend/web/js/he.js
@@ -1,10 +1,18 @@
 
 $(document).ready(function() {
-	$('.element1').addClass('bounce');
+	setInterval(function() {
+  $('.element1').addClass('bounce');
 	window.setTimeout(function(){
         $('.element2').addClass('bounce');
     }, 1500);
     window.setTimeout(function(){
         $('.element3').addClass('bounce');
     }, 3000);
+    window.setTimeout(function(){
+        $('.element1').removeClass('bounce');
+        $('.element2').removeClass('bounce');
+        $('.element3').removeClass('bounce');
+    }, 5000);
+}, 7000);
+		
 });

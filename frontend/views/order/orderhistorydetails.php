8551f91186ff394dbb2d793c51af079b39307cbf
iShinYxR <31298177+iShinYxR@users.noreply.github.com>
2017-10-06 10:53:58 +0800

    xD

--- frontend/views/order/orderhistorydetails.php
+++ frontend/views/order/orderhistorydetails.php
@@ -6,41 +6,48 @@ use common\models\Orderitemselection;
 use common\models\food\Foodselection;
 use common\models\food\Foodselectiontype;
 use common\models\Orders;
+use common\models\Ordersstatuschange;
+use common\models\User\Userdetails;
 use yii\helpers\Html;
+
 ?>
 
 <div class = "container">
     <div>
         <?php echo "<h1><center> Invoice </h1>";
+        $did = Orders::find()->where('Delivery_ID = :did',[':did'=>$did])->one();
+        $details = Userdetails:: find()->where('User_Username = :uname',[':uname'=>$did['User_Username']])->one();
         echo "<br>";
         echo "<br>";
-        echo "<table class= table table-user-info style= width:80%;>";
+        echo "<table class= table table-user-info style= width:100%;>";
+            echo "<tr><td> SGShop Ecommerce Sdn Bhd </td></tr>";
+            echo "<tr><td> 1123326T </td></tr>";
+            echo "<tr><td> B-GF-05, Medini 6, Jalan Medini Sentral 5, Bandar Medini Iskandar Malaysia, 79250 Iskandar Puteri, Johor, Malaysia. </td></tr>";
+        echo "</table>";
+        echo "<br>";
+
+        echo "<table class= table table-user-info style= width:100%;>";
             echo "<tr>";
-                echo "<th><center> Address </th>";
-                echo "<td><center> $address </td>";
-                echo "<th><center> Status </th>";
-                echo "<td><center> $status </td>";
+                echo "<td><strong>Username: </strong>".$did['User_Username']."</td>";
+                echo "<td><strong>Delivery ID: </strong>".$did['Delivery_ID']."</td>";
             echo "</tr>";
             echo "<tr>";
-                echo "<th><center> Receiving Date </th>";
-                echo "<td><center> $date </td>";
-                echo "<th><center> Receiving Time </th>";
-                echo "<td><center> $time </td>";
+                echo "<td><strong> Mobile No: </strong>".$details['User_ContactNo']."</td>";
+                echo "<td><strong> Payment Mode: </strong>".$did['Orders_PaymentMethod']."</td>";
             echo "</tr>";
             echo "<tr>";
-                echo "<th><center> Payment Method </th>";
-                echo "<td><center> $paymethod </td>";
-                echo "<th><center> Time Placed </th>";
-                echo "<td><center> $timeplaced </td>";
+                echo "<td><strong> Delivery Address: </strong>".$did['Orders_Location'].', '.$did['Orders_Area'].', '.$did['Orders_Postcode'].'.'."</td>";
+                $timepay = Ordersstatuschange::find()->where('Delivery_ID = :did', [':did'=>$did['Delivery_ID']])->one();
+                echo "<td><strong> Payment On: </strong>".date('d/m/Y H:i:s', $timepay['OChange_CompletedDateTime'])."</td>";
             echo "</tr>";
         echo "</table>";
         echo "<br>";
         echo "<br>";
 
-        echo "<table class= table table-user-info style= width:80%;>";
+        echo "<table class= table table-user-info style= width:100%;>";
             echo "<tr>";
             echo "<th><center>Order ID</th>";
-                echo "<th colspan = 2><center>Food Name</th>";
+                echo "<th><center>Food Name</th>";
                 echo "<th><center>Unit Price (RM)</th>";
                 echo "<th><center>Quantity</th>";
                 echo "<th><center>Selections</th>";
@@ -55,8 +62,7 @@ use yii\helpers\Html;
             ?>
             
             <?php
-            echo "<td><center>".$orderitemdetails['Order_ID']."</td>"; ?>
-            <td><center><?php echo Html::img('@web/imageLocation/'.$fooddetails['PicPath'], ['class' => 'img-responsive','style'=>'height:60px; width:90px; margin:auto;']); ?></td><?php
+            echo "<td><center>".$orderitemdetails['Order_ID']."</td>";
             echo "<td><center>".$fooddetails['Name']."</td>";
             echo "<td align="."right>".$fooddetails['Price']."</td>";
             echo "<td><center>".$orderitemdetails['OrderItem_Quantity']."</td>";
@@ -78,7 +84,7 @@ use yii\helpers\Html;
             echo "<td colspan = 2><center>".$orderitemdetails['OrderItem_Remark']."</td>";
             echo "</tr>";
           endforeach;
-          $did = Orders::find()->where('Delivery_ID = :did',[':did'=>$did])->one();
+          
           //var_dump($did);exit;
           echo "<tr>";
             echo "<td> </td>";
@@ -110,10 +116,23 @@ use yii\helpers\Html;
             echo "<td> </td>";
             echo "<td> </td>";
             echo "<td> </td>";
+            echo "<td><center><strong> Discount (RM): </strong></td>";
+            echo "<td align="."right>".$did['Orders_DiscountTotalAmount']."</td>";
+          echo "</tr>";
+          echo "<tr>";
+            echo "<td> </td>";
+            echo "<td> </td>";
+            echo "<td> </td>";
+            echo "<td> </td>";
+            echo "<td> </td>";
+            echo "<td> </td>";
+            echo "<td> </td>";
             echo "<td><center><strong> Total (RM): </strong></td>";
             echo "<td align="."right><strong>".$did['Orders_TotalPrice']."</strong></td>";
           echo "</tr>";
           echo "</table>";
+
+
         ?>
     </div>
 </div>

62ee535a92a13e615b559ae27319991fc02d3b82
ding94 <dw-ding@hotmail.co.uk>
2017-10-12 11:40:23 +0800

    Change Food Junction edit format

--- backend/modules/Restaurant/views/food/detail.php
+++ backend/modules/Restaurant/views/food/detail.php
@@ -10,60 +10,61 @@ use iutbay\yii2fontawesome\FontAwesome as FA;
 
 ?>
     <h4><?= Html::encode("Food Selection") ?></h4>
-    <?php
-        $dataProvider = new ArrayDataProvider([
-            'allModels' => $model,
-        ]); 
-      ?>
-    <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'columns' => [
-            [
-                'attribute' => 'typeName',
-                'value' => function($model)
-                {
-                    return $model->selectedtpye->TypeName;
-                },
-                'group'=>true,
-            ],
-            [
-                'attribute' => 'min',
-                'value' => function($model)
-                {
-                    return $model->selectedtpye->Min;
-                },
-                'group'=>true,
-            ],
-            [
-                'attribute' => 'max',
-                'value' => function($model)
-                {
-                    return $model->selectedtpye->Max;
-                },
-                'group'=>true,
-            ],
-            'Name',
-            'Nickname',
-            'BeforeMarkedUp',
-            'Price',
-            [
-                'attribute' => 'tpyeStatus',
-                'format' => 'raw',
-                'value' => function($model)
-                {
-                    if($model->Status == 0)
+    <?php if(!empty($model)) : ?>
+        <?php 
+            $dataProvider = new ArrayDataProvider([
+                'allModels' => $model,
+            ]);
+        ?>
+        <?= GridView::widget([
+            'dataProvider' => $dataProvider,
+            'columns' => [
+                [
+                    'attribute' => 'typeName',
+                    'value' => function($model)
                     {
-                        $url =Url::to(['food/type-control','id' =>$model->ID ,'status' => 1]);
-                    }
-                    else
+                        return $model->selectedtpye->TypeName;
+                    },
+                    'group'=>true,
+                ],
+                [
+                    'attribute' => 'min',
+                    'value' => function($model)
                     {
-                        $url =Url::to(['food/type-control','id' =>$model->ID ,'status' => 0]);
-                    }
-                    return $model->Status == 0 ?  Html::a(FA::icon('toggle-off lg') , $url , ['title' => 'ON']) :  Html::a(FA::icon('toggle-on lg') , $url , ['title' => 'OFF']);
-                },
-                'filter' =>  array( 0=>"Close",1=>"Open"),
+                        return $model->selectedtpye->Min;
+                    },
+                    'group'=>true,
+                ],
+                [
+                    'attribute' => 'max',
+                    'value' => function($model)
+                    {
+                        return $model->selectedtpye->Max;
+                    },
+                    'group'=>true,
+                ],
+                'Name',
+                'Nickname',
+                'BeforeMarkedUp',
+                'Price',
+                [
+                    'attribute' => 'tpyeStatus',
+                    'format' => 'raw',
+                    'value' => function($model)
+                    {
+                        if($model->Status == 0)
+                        {
+                            $url =Url::to(['food/type-control','id' =>$model->ID ,'status' => 1]);
+                        }
+                        else
+                        {
+                            $url =Url::to(['food/type-control','id' =>$model->ID ,'status' => 0]);
+                        }
+                        return $model->Status == 0 ?  Html::a(FA::icon('toggle-off lg') , $url , ['title' => 'ON']) :  Html::a(FA::icon('toggle-on lg') , $url , ['title' => 'OFF']);
+                    },
+                    'filter' =>  array( 0=>"Close",1=>"Open"),
+                ],
             ],
-        ],
-    ]); ?>
-
-
+        ])
+        ?>
+    <?php endif ; ?>

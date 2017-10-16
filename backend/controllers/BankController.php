4930e02de13abe78b5a5fb18fd58313469cd22f4
lene1994 <lene.ng94@gmail.com>
2017-10-16 17:23:20 +0800

    backend Bank added

---
+++ backend/controllers/BankController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace backend\controllers;
+use common\models\Upload;
+use common\models\Bank;
+use Yii;
+use yii\data\ActiveDataProvider;
+use yii\web\UploadedFile;
+use yii\helpers\Url;
+
+class BankController extends \yii\web\Controller
+{
+    public function actionIndex()
+    {
+        $searchModel = new Bank();
+       	$dataProvider = $searchModel->search(Yii::$app->request->queryParams);
+		return $this->render('index',['model' => $dataProvider]);
+    }
+		public function actionAddbank()
+	{
+		$model = new Bank();
+		$upload = new Upload();
+		//$path = Yii::$app->params['imageLocation'];
+		$path = Yii::$app->request->baseUrl.'/imageLocation';
+		if(Yii::$app->request->post())
+		{
+			$post = Yii::$app->request->post();
+			$upload->imageFile =  UploadedFile::getInstance($upload, 'imageFile');
+    		$upload->imageFile->name = time().'.'.$upload->imageFile->extension;
+    		$model->Bank_PicPath = $path.'/'.$upload->imageFile->name;
+
+    		if($upload->validate())
+    		{
+				$imageName = $upload->imageFile->name;
+				$imagePath = Url::to('@frontend/web/imageLocation/bank/').$imageName;
+    			$upload->imageFile->saveAs($imagePath);
+    			$model->load($post);
+				$model->Bank_PicPath = $imageName;
+				//var_dump($model);exit;
+    			$model->save();
+    			Yii::$app->session->setFlash('success', 'Upload Successful');
+    		} else{
+    			Yii::$app->session->setFlash('danger', 'Upload Fail');
+    			return $this->redirect(['index']);
+    		}
+		}$model->Bank_Name ="";
+		$model->Bank_AccNo ="";
+		$model->redirectUrl ="";
+		return $this->render('addbank',['upload' => $upload, 'model' => $model]);
+	}
+	public function actionUpdate($id)
+    {
+		 $model = Bank::find()->where('Bank_ID = :Bank_ID',[':Bank_ID'=>$id])->one();
+			if($model->load(Yii::$app->request->post()))
+			{
+				
+				$model->save();
+			//	self::updateAllTopup();
+				Yii::$app->session->setFlash('success', "Update success");
+				return $this->redirect(['index']);
+			}
+			else{
+				//Yii::$app->session->setFlash('error', "Fail to Update");
+				return $this->render('update', ['model' => $model]);
+			}
+			return $this->redirect(['index']); 
+		}
+		
+	public function actionActive($id)
+	{
+		$model = Bank::find()->where('Bank_ID = :Bank_ID',[':Bank_ID'=>$id])->one();
+		$model->status = 10;
+		if($model->update(false) !== false)
+		{
+			Yii::$app->session->setFlash('success', "Activate Sucessfully");
+		}
+		else{
+			Yii::$app->session->setFlash('warning', "Fail to Activate");
+		}
+        return $this->redirect(['index']);
+
+	}
+	public function actionDeactivate($id)
+	{
+		$model = Bank::find()->where('Bank_ID = :Bank_ID',[':Bank_ID'=>$id])->one();
+		$model->status = 0;
+		if($model->update(false) !== false)
+		{
+			Yii::$app->session->setFlash('warning', "Deactivate completed");
+		}
+		else{
+			Yii::$app->session->setFlash('warning', "Fail to deactivate");
+		}
+       return $this->redirect(Yii::$app->request->referrer);
+	}
+	public function actionDelete($id)
+	{
+		$model = Bank::find()->where('Bank_ID = :Bank_ID',[':Bank_ID'=>$id])->one();
+		if ($model) {
+			$model->delete();
+		}
+		return $this->redirect(['index']);
+	}
+}
---
+++ backend/views/bank/update.php
@@ -0,0 +1,28 @@
+<?php
+
+/* @var $this yii\web\View */
+/* @var $form yii\bootstrap\ActiveForm */
+/* @var $model \common\models\LoginForm */
+
+use yii\helpers\Html;
+use kartik\widgets\ActiveForm;
+
+	//var_dump($model);exit;
+	$this->title = 'Bank';
+	$this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Bank'), 'url' => ['index']];
+	$this->params['breadcrumbs'][] = $this->title;
+?>
+
+
+	<?php $form = ActiveForm::begin();?>
+	
+		
+    <?= $form->field($model, 'Bank_Name')->textInput(['readonly' => true]) ?>
+	<?= $form->field($model, 'Bank_AccNo')->textInput() ?>
+	<?= $form->field($model, 'redirectUrl')->textInput() ?>
+	
+	    	
+    	<div class="form-group">
+	        <?= Html::submitButton('Update', ['class' => 'btn btn-primary']) ?>
+	   </div>
+	<?php ActiveForm::end();?>

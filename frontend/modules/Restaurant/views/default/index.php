95984b8f5fb71fa9356d329b282deaf9249bb744
shijuin97 <ngshijuin@gmail.com>
2017-10-19 14:52:51 +0800

    filter restaurant UI

--- frontend/modules/Restaurant/views/default/index.php
+++ frontend/modules/Restaurant/views/default/index.php
@@ -71,15 +71,68 @@ span.stars span {
 .menu-container a:hover{
     box-shadow: 0px 0px 20px -2px grey;
 }
-
+.filter{
+    height:auto;
+    width:230px;
+    float:left;
+   margin-left:-10px;
+}
+.filter.container{
+     border: 1px solid #e5e5e5;
+    padding: 20px;
+    margin-top:26px;
+    
+}
+.filter-list{
+    list-style: none;
+    font-size:13px;
+    letter-spacing: .5px;
+    line-height:80%;
+    padding-left:28px;
+}
+.filter.name p{
+    margin-left:11px;
+    letter-spacing: .5px;
+    font-size:15px;
+}
+.filter.list a:hover{
+     text-decoration:none;
+}
+.input-group{
+        position: relative;
+    display: table;
+    border-collapse: separate;
+}
+.input-group-btn {
+    position: relative;
+    font-size: 0;
+    white-space: nowrap;
+}
 </style>
 <div class="container" id="index">
     <h1>Order Food for Delivery</h1>
+    <div class="filter">
     <?php echo Html::a('<i class="fa fa-cutlery"> Food</i>', ['show-by-food', 'groupArea'=>$groupArea], ['class'=>'btn btn-default']); ?>
+     <div class="filter container">
+    <div class="input-group">
+    <input class="form-control" type="text" name="SearchBar" value="" placeholder="Search Food Name">
+    <div class="input-group-btn">
+    <button class="btn btn-default" type="submit" name="SearchBtn"><i class="fa fa-search"></i></button>
+ 
+    </div>
+    </div>
+     <br>
+        <div class ="filter name">
+        <p><i class="fa fa-sliders"> Filter By</i></p>
+     </div>
+    <ul class ="filter-list">
     <?php echo Html::a('<li>All</li>', ['index', 'groupArea'=>$groupArea])."&nbsp;&nbsp;"; ?>
     <?php foreach ($types as $types) :
             echo Html::a('<li>'.$types['Type_Name'].'</li>', ['restaurant-filter', 'groupArea'=>$groupArea ,'rfilter'=>$types['ID']])."&nbsp;&nbsp;";
           endforeach; ?>
+                   </ul>
+          </div>
+          </div>
 
     <div class="outer-container">
       <div class="menu-container">

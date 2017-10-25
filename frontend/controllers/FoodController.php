4eda33ff44c4d0194fb5635fbf6353a3dfbc9085
soonws <soonws97@gmail.com>
2017-10-25 15:08:37 +0800

    rmanager manger restaurant and food function

--- frontend/controllers/FoodController.php
+++ frontend/controllers/FoodController.php
@@ -37,7 +37,7 @@ class FoodController extends CommonController
                 //'only' => ['foodDetails', 'insertFood','menu','delete','editFood','postedit','recycleBin','deletePermanent','viewComments'],
                 'rules' => [
                     [
-                        'actions' => [ 'insertFood','menu','delete','edit-food','postedit','recycleBin','deletePermanent','viewComments'],
+                        'actions' => [ 'insert-food','menu','delete','edit-food','postedit','recycleBin','delete-permanent','viewComments'],
                         'allow' => true,
                         'roles' => ['restaurant manager'],
                     ],

89bb41315b7c0bbe38687c432156981fa7bb96ad
iShinYxR <31298177+iShinYxR@users.noreply.github.com>
2017-10-27 15:57:58 +0800

    FAQ

--- frontend/views/site/faq.php
+++ frontend/views/site/faq.php
@@ -3,14 +3,39 @@
 <div class = "container">
     <h1><strong>Frequently Asked Questions</strong></h1>
     <br>
-    <ul>
-        <li>My postcode is not listed. What should I do?</li>
-        <li>Our services are currently expanding. Do wait until your area is available.</li>
+    <ul style="list-style-type:none">
+        <li>Q: What if I encounter a problem with my order?</li>
+        <li>A: Kindly contact our customer service and provide your Delivery ID before describing your problem.</li>
         <br>
-        <li>Can I cancel an order that I have already placed?</li>
-        <li>No, you cannot cancel an order that is successfully placed.</li>
+        <li>Q: My postcode is not listed. What should I do?</li>
+        <li>A: Our services are currently expanding. Do wait until your area is available.</li>
         <br>
-        <li>Where can I see my ongoing orders?</li>
-        <li>You can see your ongoing orders under <strong>My Profile > My Orders</strong>.</li>
+        <li>Q: Can I cancel an order that I have already placed?</li>
+        <li>A: No, you cannot cancel an order that is successfully placed.</li>
+        <br>
+        <li>Q: Where can I see my ongoing orders or my completed orders?</li>
+        <li>A: You can see your ongoing orders under <strong>My Profile > My Orders</strong> and your completed orders under <strong>My profile > Order History</strong>.</li>
+        <br>
+        <li>Q: Can I withdraw my account balance?</li>
+        <li>A: Yes, you may withdraw the balance left in your account, but we will be deducting RM 2.00 as procedure fee.</li>
+        <br>
+        <li>Q: Can I upgrade my account to a Restaurant Manager or a Delivery Man from a member account?</li>
+        <li>A: Yes, you may upgrade your account to either a Restaurant Manager or a Delivery Man but you may not be both of that. A Restaurant Manager or Delivery Man account has the features of a member account.</li>
+        <br>
+        <li>Q: Can i downgrade my account from a Delivery Man / Restaurant Manager to a member account?</li>
+        <li>A: No, you cannot downgrade your account.</li>
+        <br>
+        <li>Q: Why can't I place my order before 9:30am and after 11:00am daily?</li>
+        <li>A: We are currently still in our beta phase, thus we only offer lunch deliveries for now.</li>
+        <br>
+        <li>Q: What if I want to avoid certain ingredients in my order due to allergies etc.?</li>
+        <li>A: Kindly write down what you would like to avoid in the remarks section provided.</li>
+        <br>
+        <li>Q: Is there a minimum fee I have to hit in order to place an order?</li>
+        <li>A: Yes, your order must be at least RM 10.00 excluding delivery charge in order to place your order.</li>
+        <br>
+        <li>Q: How do I use the voucher or discount codes I received?</li>
+        <li>A: You can use the codes before you check out in the voucher / discount code field provided.</li>
+        
     </ul>
 </div>

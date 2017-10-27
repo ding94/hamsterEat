d8c6ff1c3fce07ccceacfd14aea938e543635202
iShinYxR <31298177+iShinYxR@users.noreply.github.com>
2017-10-27 16:10:56 +0800

    Add ons

--- frontend/views/site/faq.php
+++ frontend/views/site/faq.php
@@ -4,38 +4,46 @@
     <h1><strong>Frequently Asked Questions</strong></h1>
     <br>
     <ul style="list-style-type:none">
-        <li>Q: What if I encounter a problem with my order?</li>
-        <li>A: Kindly contact our customer service and provide your Delivery ID before describing your problem.</li>
+        <li><strong>Q:</strong> What if I encounter a problem with my order?</li>
+        <li><strong>A:</strong> Kindly contact our customer service and provide your Delivery ID before describing your problem.</li>
         <br>
-        <li>Q: My postcode is not listed. What should I do?</li>
-        <li>A: Our services are currently expanding. Do wait until your area is available.</li>
+        <li><strong>Q:</strong> My postcode is not listed. What should I do?</li>
+        <li><strong>A:</strong> Our services are currently expanding. Do wait until your area is available.</li>
         <br>
-        <li>Q: Can I cancel an order that I have already placed?</li>
-        <li>A: No, you cannot cancel an order that is successfully placed.</li>
+        <li><strong>Q:</strong> Can I cancel an order that I have already placed?</li>
+        <li><strong>A:</strong> No, you cannot cancel an order that is successfully placed.</li>
         <br>
-        <li>Q: Where can I see my ongoing orders or my completed orders?</li>
-        <li>A: You can see your ongoing orders under <strong>My Profile > My Orders</strong> and your completed orders under <strong>My profile > Order History</strong>.</li>
+        <li><strong>Q:</strong> Where can I see my ongoing orders or my completed orders?</li>
+        <li><strong>A:</strong> You can see your ongoing orders under <strong>My Profile > My Orders</strong> and your completed orders under <strong>My profile > Order History</strong>.</li>
         <br>
-        <li>Q: Can I withdraw my account balance?</li>
-        <li>A: Yes, you may withdraw the balance left in your account, but we will be deducting RM 2.00 as procedure fee.</li>
+        <li><strong>Q:</strong> Can I withdraw my account balance?</li>
+        <li><strong>A:</strong> Yes, you may withdraw the balance left in your account, but we will be deducting RM 2.00 as procedure fee.</li>
         <br>
-        <li>Q: Can I upgrade my account to a Restaurant Manager or a Delivery Man from a member account?</li>
-        <li>A: Yes, you may upgrade your account to either a Restaurant Manager or a Delivery Man but you may not be both of that. A Restaurant Manager or Delivery Man account has the features of a member account.</li>
+        <li><strong>Q:</strong> Can I upgrade my account to a Restaurant Manager or a Delivery Man from a member account?</li>
+        <li><strong>A:</strong> Yes, you may upgrade your account to either a Restaurant Manager or a Delivery Man but you may not be both of that. A Restaurant Manager or Delivery Man account has the features of a member account.</li>
         <br>
-        <li>Q: Can i downgrade my account from a Delivery Man / Restaurant Manager to a member account?</li>
-        <li>A: No, you cannot downgrade your account.</li>
+        <li><strong>Q:</strong> Can i downgrade my account from a Delivery Man / Restaurant Manager to a member account?</li>
+        <li><strong>A:</strong> No, you cannot downgrade your account.</li>
         <br>
-        <li>Q: Why can't I place my order before 9:30am and after 11:00am daily?</li>
-        <li>A: We are currently still in our beta phase, thus we only offer lunch deliveries for now.</li>
+        <li><strong>Q:</strong> Why can't I place my order before 9:30am and after 11:00am daily?</li>
+        <li><strong>A:</strong> We are currently still in our beta phase, thus we only offer lunch deliveries for now.</li>
         <br>
-        <li>Q: What if I want to avoid certain ingredients in my order due to allergies etc.?</li>
-        <li>A: Kindly write down what you would like to avoid in the remarks section provided.</li>
+        <li><strong>Q:</strong> What if I want to avoid certain ingredients in my order due to allergies etc.?</li>
+        <li><strong>A:</strong> Kindly write down what you would like to avoid in the remarks section provided.</li>
         <br>
-        <li>Q: Is there a minimum fee I have to hit in order to place an order?</li>
-        <li>A: Yes, your order must be at least RM 10.00 excluding delivery charge in order to place your order.</li>
+        <li><strong>Q:</strong> Is there a minimum fee I have to hit in order to place an order?</li>
+        <li><strong>A:</strong> Yes, your order must be at least RM 10.00 excluding delivery charge in order to place your order.</li>
         <br>
-        <li>Q: How do I use the voucher or discount codes I received?</li>
-        <li>A: You can use the codes before you check out in the voucher / discount code field provided.</li>
-        
+        <li><strong>Q:</strong> How do I use the voucher or discount codes I received?</li>
+        <li><strong>A:</strong> You can use the codes before you check out in the voucher / discount code field provided.</li>
+        <br>
+        <li><strong>Q:</strong> How do I invite a friend to join HamsterEat?</li>
+        <li><strong>A:</strong> You can provide your friend your referral link which he can use to sign up an account and get a RM 5.00 voucher which the referrer will get as well.</li>
+        <br>
+        <li><strong>Q:</strong> How does HamsterEat differentiate areas?</li>
+        <li><strong>A:</strong> We mainly differentiate areas based on postcodes. However, under certain circumstances where the postcode areas are too big, we will divide it into smaller parts.</li>
+        <br>
+        <li><strong>Q:</strong> Do I have to tip my delivery man?</li>
+        <li><strong>A:</strong> Tipping is not necessary but it is up to the customer's own free will.</li>
     </ul>
 </div>

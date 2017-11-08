ccb8177c88c8e18a8426c79f0dd4f3d540f421f8
iShinYxR <31298177+iShinYxR@users.noreply.github.com>
2017-11-08 12:25:38 +0800

    Delivery Location Display

--- frontend/modules/delivery/views/daily-sign-in/deliverylocation.php
+++ frontend/modules/delivery/views/daily-sign-in/deliverylocation.php
@@ -4,6 +4,7 @@
 use kartik\widgets\ActiveForm;
 use yii\helpers\Html;
 use kartik\widgets\Select2;
+use common\models\Area;
 
 $this->title = 'Delivery Location';
 ?>
@@ -13,29 +14,38 @@ $this->title = 'Delivery Location';
 		<div class="tab-content col-md-6 col-md-offset-3" >
 		<table class="table table-user-information"> <h1>Set Up Your Delivery Location</h1>
       <?php
-       echo "<tr>";
-                        echo "<th ><center> Area Group </th>";
-                     
-                        echo "<th><center> Area </th>";
-                         echo "<th><center> Postcode </th>";
-                          echo "<th ><center> State</th>";
-         
-                 
-                    echo "</tr>";
+        echo "<tr>";
+        echo "<th ><center> Area Group </th>";
+        echo "<th><center> Area </th>";
+        echo "<th><center> Postcode </th>";
+        echo "<th ><center> State</th>";
+        echo "</tr>";
       ?>
       <?php
       foreach ($area as $area) :
-        echo "<tr>";
-        if ($area['Area_Group'] == $area['Area_Group'])
-        {
-            echo "<td><center>".$area['Area_Group']."</td>";
-        }
-        echo "<td><center>".$area['Area_Area']."</td>";
-        echo "<td><center>".$area['Area_Postcode']."</td>";
-        echo "<td><center>".$area['Area_State']."</td>";
-        echo "</tr>";
-
-        endforeach;
+        $amount = Area::find()->where('Area_Group = :ag', [':ag'=>$area['Area_Group']])->all();
+        $count = count($amount);
+          foreach ($amount as $amount) :
+            if ($count > 0)
+            {
+              echo "<tr>";
+                echo "<td rowspan = $count>".$amount['Area_Group']."</td>";
+                echo "<td>".$amount['Area_Area']."</td>";
+                echo "<td>".$amount['Area_Postcode']."</td>";
+                echo "<td>".$amount['Area_State']."</td>";
+              echo "</tr>";
+            }
+            else
+            {
+              echo "<tr>";
+                echo "<td>".$amount['Area_Area']."</td>";
+                echo "<td>".$amount['Area_Postcode']."</td>";
+                echo "<td>".$amount['Area_State']."</td>";
+              echo "</tr>";
+            }
+            $count = 0;
+          endforeach;
+      endforeach;
       ?>
  </table>
    <?php $form = ActiveForm::begin(['id' => 'dynamic-form']); ?>

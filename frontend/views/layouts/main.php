075612e6952cce6694cd9a4f7c88cc913e2b5a01
iShinYxR <31298177+iShinYxR@users.noreply.github.com>
2017-11-08 14:36:21 +0800

    Fix

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -33,7 +33,7 @@ NotificationAsset::register($this);
     #cart1{
         line-height:33px;
     }
-    .modal-content-feedback{
+    .modal-content{
         width:800px;
         margin-left: -230px;
         margin-top: 100px;

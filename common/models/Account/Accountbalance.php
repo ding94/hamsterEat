f266174bef53916fe602f1d8450656aaf9ef306b
soonws <soonws97@gmail.com>
2017-11-10 17:41:07 +0800

    accountbalance model change

--- common/models/Account/Accountbalance.php
+++ common/models/Account/Accountbalance.php
@@ -73,7 +73,8 @@ class Accountbalance extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
-            [['AB_DateTime','AB_ID'], 'integer'],
+            [['AB_ID'], 'integer'],
+            [['AB_DateTime'],'date'],
             [['AB_topup', 'AB_minus','User_Balance'],'number'],
             [['User_Username'], 'string', 'max' => 255],
             

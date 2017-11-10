4f17aaa98eb29194e4e69ad6e55628af9f40527a
soonws <soonws97@gmail.com>
2017-11-10 15:11:57 +0800

    frontend when food was closed, effected orders and deliveries will be cancel and refunded

--- common/models/Orderitem.php
+++ common/models/Orderitem.php
@@ -91,4 +91,9 @@ class Orderitem extends \yii\db\ActiveRecord
     {
         return $this->hasOne(Restaurant::className(),['Restaurant_ID' => $this->food->Restaurant_ID]);
     }
+
+    public function getProblem_Order()
+    {
+        return $this->hasOne(ProblemOrder::className(),['Order_ID' => 'Order_ID']);
+    }
 }
--- frontend/modules/Restaurant/views/restaurant/foodservice.php
+++ frontend/modules/Restaurant/views/restaurant/foodservice.php
@@ -1,5 +1,6 @@
 <?php
 use yii\helpers\Url;
+use yii\bootstrap\Modal;
 use yii\helpers\Html;
 use common\models\food\Foodstatus;
 use frontend\assets\StarsAsset;
@@ -8,6 +9,14 @@ $this->title = "Manage Foods";
 
 StarsAsset::register($this);
 FoodServiceAsset::register($this);
+
+Modal::begin([
+      'header' => '<h2 class="modal-title">Please choose delivery place</h2>',
+      'id'     => 'add-session-modal',
+      'size'   => 'modal-md',
+      'footer' => '<a href="#" class="btn btn-primary" data-dismiss="modal">Close</a>',
+]);
+Modal::end();
 ?>
 <body>
 <div class ="container" ><h1>Manage Foods</h1>
@@ -30,9 +39,9 @@ FoodServiceAsset::register($this);
       </div>
       <?php if (!empty($status)): ?>
         <?php if ($status['Status'] == 0): ?>
-              <?=Html::a('Resume Food Service', Url::to(['restaurant/active', 'id'=>$food['Food_ID'],'item'=>2]), ['id'=>'res','data-confirm'=>"Do you want to Resume Operate?",'class'=>'btn btn-warning'])?>
+              <?=Html::a('Resume Food Service', Url::to(['restaurant/active', 'id'=>$food['Food_ID']]), ['id'=>'res','data-confirm'=>"Do you want to Resume Operate?",'class'=>'btn btn-warning'])?>
         <?php elseif ($status['Status'] == 1): ?>
-              <?=Html::a('Pause Food Service', Url::to(['restaurant/deactive', 'id'=>$food['Food_ID'],'item'=>2]), ['id'=>'res','data-confirm'=>"Do you want to Pause Operate?",'class'=>'btn btn-warning'])?>  
+              <?=Html::a('Pause Food Service', Url::to(['restaurant/providereason', 'id'=>$food['Food_ID']]), ['id'=>'res','data-confirm'=>"Do you want to Pause Operate?",'class'=>'btn btn-warning','data-toggle'=>'modal','data-target'=>'#add-session-modal'])?>  
         <?php endif ?>
       <?php endif ?>
       </div>

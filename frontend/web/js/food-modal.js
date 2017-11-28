d873df0811f0379a73cd8ae4787ae935b4eec8dd
ding94 <dw-ding@hotmail.co.uk>
2017-11-28 14:00:23 +0800

    a

--- frontend/web/js/food-modal.js
+++ frontend/web/js/food-modal.js
@@ -5,7 +5,7 @@ $(function(){
             var button = $(event.relatedTarget);
             var modal = $(this);
             var href = button.attr('href');
-            var img = '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button><img class=\"img-rounded img-responsive detail-img\" src=\"./../imageLocation/foodImg/'+button.attr('data-img')+'\" alt=\"\" ">';
+            var img = '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button><img class=\"img-rounded img-responsive detail-img\" src=\"./../web/imageLocation/foodImg/'+button.attr('data-img')+'\" alt=\"\" ">';
             modal.find('.modal-body').html('<i class=\"fa fa-spinner fa-spin\"></i>');
             $.post(href)
                 .done(function( data ) {

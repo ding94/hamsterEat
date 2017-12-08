0c5df7e37f8cd944c4e27224a34a41e88ddf7087
soonws <soonws97@gmail.com>
2017-12-08 10:38:42 +0800

    added validation for user duplicate company

--- backend/controllers/CompanyController.php
+++ backend/controllers/CompanyController.php
@@ -11,6 +11,7 @@ use common\models\User;
 use common\models\Company\Company;
 use common\models\Company\CompanyEmployees;
 use yii\web\UploadedFile;
+use common\models\Object;
 
 class CompanyController extends Controller
 {
@@ -27,20 +28,29 @@ class CompanyController extends Controller
     {
     	$company = new Company();
     	$company->scenario = 'register';
-        $postcode = ArrayHelper::map(Area::find()->all(),'Area_ID','Area_Postcode');
+        $postcode = ArrayHelper::map(Area::find()->all(),'Area_Postcode','Area_Postcode');
 
         if (Yii::$app->request->post()) {
             $company->load(Yii::$app->request->post());
             $company['status'] = 1;
-            $owner = User::find()->where('username=:u',[':u'=>$company['username']])->one();
+            
+            $owner = User::find()->where('username=:u',[':u'=>$company['username']])->one();//get owner data by username
+
             if (!empty($owner)) {
+                //if the username was an owner of another company, set flase
+                if (CompanyEmployees::find()->where('uid=:uid',[':uid'=>$owner['id']])->one()) {
+                    Yii::$app->session->setFlash('error','User was other company owner!');
+                    return $this->render('register',['company'=>$company,'postcode'=>$postcode]);
+                }
+                //else setup owner id
                 $company['owner_id'] = $owner['id'];
             }
             else{
+                //if username connot be found
                 Yii::$app->session->setFlash('error','Fail to found user!');
-                return $this->render('register',['company'=>$company]);
+                return $this->render('register',['company'=>$company,'postcode'=>$postcode]);
             }
-
+            
             if ($company->validate()) {
                 Yii::$app->session->setFlash('success','Success!');
                 $company->save();
@@ -55,6 +65,34 @@ class CompanyController extends Controller
         }
     	 
     	return $this->render('register',['company'=>$company,'postcode'=>$postcode]);
+    }
+
+    public function actionGetArea()
+    {
+        if (isset($_POST['depdrop_parents'])) {
+            $parents = $_POST['depdrop_parents'];
+            if ($parents != null) {
+                $cat_id = $parents[0];
+                $out = self::getAreaList($cat_id); 
+                echo json_encode(['output'=>$out, 'selected'=>'']);
+                return;
+            }
+        }
+        echo json_encode(['output'=>'', 'selected'=>'']);
+    }
+
+    public static function getAreaList($postcode)
+    {
+        $area = Area::find()->where(['like','Area_Postcode' , $postcode])->select(['Area_ID', 'Area_Area'])->all();
+        $areaArray = [];
+        foreach ($area as $area) {
+            $object = new Object();
+            $object->id = $area['Area_Area'];
+            $object->name = $area['Area_Area'];
+
+            $areaArray[] = $object;
+        }
+        return $areaArray;
     }
 
     public function actionEdit($id)
--- backend/views/company/register.php
+++ backend/views/company/register.php
@@ -1,6 +1,9 @@
 <?php
 use yii\helpers\Html;
 use kartik\widgets\ActiveForm;
+use kartik\widgets\Select2;
+use kartik\widgets\DepDrop;
+use yii\helpers\Url;
 
 	$this->title = 'Register new Comapny';
 	$this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Company List'), 'url' => ['/company/index']];
@@ -13,8 +16,21 @@ use kartik\widgets\ActiveForm;
     <?= $form->field($company ,'username')->textInput()->label('Owner username')?>
     <?= $form->field($company ,'license_no')->textInput()?>
     <?= $form->field($company ,'address')->textInput()->label('Company address')?>
-    <?= $form->field($company ,'postcode')->dropDownList($postcode)?>
-    <?= $form->field($company ,'area')->textInput()?>
+
+
+    <?= $form->field($company, 'postcode')->widget(Select2::classname(), [
+        'data' => $postcode,
+        'options' => ['placeholder' => 'Select a postcode ...','id'=>'postcode-select']])->label('Postcode');
+        ?>
+
+    <?= $form->field($company,'area')->widget(DepDrop::classname(), [
+            'type'=>DepDrop::TYPE_SELECT2,
+            'options' => ['id'=>'area-select','placeholder' => 'Select an area ...'],
+            'pluginOptions'=>[
+                'depends'=>['postcode-select'],
+                'url'=>Url::to(['/company/get-area'])
+            ],
+            ]); ?>
 
     	<div class="form-group">
 	        <?= Html::submitButton('Register', ['class' => 'btn btn-success']) ?>

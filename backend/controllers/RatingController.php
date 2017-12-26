d6ed973a3b76075499ab4f5b038dd669697eb5fa
darrensoap <darrensoap@gmail.com>
2017-12-26 17:10:57 +0800

    restaurant average rating

--- backend/controllers/RatingController.php
+++ backend/controllers/RatingController.php
@@ -7,6 +7,7 @@ use common\models\Rating\RatingSearch;
 use common\models\Order\DeliveryAddress;
 use common\models\Rating\Servicerating;
 use common\models\Rating\Foodrating;
+use common\models\food\Food;
 use backend\controllers\CommonController;
 
 Class RatingController extends Controller
@@ -36,6 +37,23 @@ Class RatingController extends Controller
 		return $this->render('rating-stats',['data'=>$data,'arrayType'=>$arrayType,'year'=>$year,'type'=>$type]);
 	}
 
+	public function actionAverageRestaurantRatingStats($year = 0, $type = 0, $rid)
+	{
+		if ($year == 0){
+			$year = date('Y',strtotime('this year'));
+		}
+		$months = CommonController::getYear($year);
+		$startend = CommonController::getStartEnd($year);
+		foreach ($startend as $key => $value) {
+			$ratingpermonth = self::calRatingForRestaurantPerMonth($value[0],$value[1],$rid);
+			$ratingthisyear[] = $ratingpermonth;
+		}
+		$data['months'] = $months;
+		$data['allrating'] = $ratingthisyear;
+		$arrayType = [0=>'bar','1'=>'horizontalBar','2'=>'line'];
+		return $this->render('rating-stats',['data'=>$data,'arrayType'=>$arrayType,'year'=>$year,'type'=>$type]);
+	}
+
 	public static function calRatingForFoodPerMonth($start,$end,$fid)
 	{
 		$food = Foodrating::find()->select(['FoodRating_Rating'])->where('Food_ID=:fid',[':fid'=>$fid])->andWhere(['between','created_at',$start,$end])->asArray()->all();
@@ -56,6 +74,22 @@ Class RatingController extends Controller
 		} else {
 			$averagerating = 0;
 		}
+		return $averagerating;
+	}
+
+	public static function calRatingForRestaurantPerMonth($start,$end,$rid)
+	{
+		$food = Food::find()->select(['Food_ID'])->where('Restaurant_ID=:rid',[':rid'=>$rid])->asArray()->all();
+		foreach ($food as $key => $value) {
+			$foodrating[] = self::calRatingForFoodPerMonth($start,$end,$value['Food_ID']);
+		}
+		$count = 0;
+		$totalrating = 0;
+		foreach ($foodrating as $key => $value) {
+			$count += 1;
+			$totalrating += $value;
+		}
+		$averagerating = $totalrating/$count;
 		return $averagerating;
 	}
 
--- backend/views/rating/rating-stats.php
+++ backend/views/rating/rating-stats.php
@@ -8,7 +8,7 @@ use kartik\widgets\Select2;
 use kartik\widgets\ActiveForm;
 use yii\helpers\Html;
 
-$this->title = 'Food Rating Per Month';
+$this->title = 'Average Food Rating Per Month';
 $fid = Yii::$app->request->get('fid');
 ?>
 <div class="site-index">
@@ -42,7 +42,7 @@ $fid = Yii::$app->request->get('fid');
             ]);?>
         </div>
         <div class="col-md-3">
-            <?= Html::submitButton('Filter', ['class' => 'btn-block ']) ?>
+            <?= Html::submitButton('Filter', ['class' => 'btn-block']) ?>
         </div>
      </div>
 <?php ActiveForm::end(); ?> 

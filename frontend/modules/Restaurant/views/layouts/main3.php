cfdc5146f6894fb5b655d34b41dd9a0fb8f8364e
darrensoap <darrensoap@gmail.com>
2017-12-28 15:20:06 +0800

    typo error

--- frontend/modules/Restaurant/views/layouts/main3.php
+++ frontend/modules/Restaurant/views/layouts/main3.php
@@ -202,7 +202,7 @@ NotificationAsset::register($this);
                     <li><?php echo Html::a('<span class="glyphicon glyphicon-log-in"></span>',['/site/login-popup'],['data-toggle'=>'modal','data-target'=>'#login-modal']);?></li>
                     <?php } elseif(Rmanager::find()->where('uid=:id',[':id'=>Yii::$app->user->identity->id])->one()) { ?>
                     <li><?php echo Html::a('<span class="glyphicon glyphicon-shopping-cart cart"><span class="badge">'.Yii::$app->params['countCart'].'</span></span>',['/cart/view-cart']);?></li>
-                    <li><?php echo Html::a('<span class=""><i class="fa fa-bell"></i>'.Yii::$app->vparams['countNotic'].'</span>',['/notification/index']);?></li>
+                    <li><?php echo Html::a('<span class=""><i class="fa fa-bell"></i>'.Yii::$app->params['countNotic'].'</span>',['/notification/index']);?></li>
                     <li><?php echo Html::a('<i class="fa fa-cutlery"></i>',['/Restaurant/restaurant/restaurant-service']);?></li>
                     <li><?php echo Html::a('<span class="glyphicon glyphicon-user">',['/user/user-profile']);?></li>
                     <li><?php echo Html::a('<i class="fa fa-bars"></i>','#',['id'=>'toggle-menu-test']);?></li>

8582255554f98cddb1081fe0c8d2d20f6f8d5271
soonws <soonws97@gmail.com>
2017-12-29 16:49:57 +0800

    edit food halal block

--- frontend/views/food/insertfood.php
+++ frontend/views/food/insertfood.php
@@ -43,7 +43,7 @@ AddFoodAsset::register($this);
                 ])->textInput(['id'=>'price'])->label("Money Received");?>
 
                 <?php if($rtype==$nonhalal['Type_Desc']): ?>
-                    <?= $form->field($foodjunction, 'Type_ID')->radioList([$halal['ID']=>$halal['Type_Name'],$nonhalal['ID']=>$nonhalal['Type_Name']])->label('<strong>Type</strong>') ?>
+                    <?= $form->field($foodjunction, 'Type_ID')->radioList([$halal['ID']=>$halal['Type_Desc'],$nonhalal['ID']=>$nonhalal['Type_Desc']])->label('<strong>Type</strong>') ?>
                 <?php else:?>
                     <?php $foodjunction['Type_ID']=$halal['ID']; ?>
                     <?= $form->field($foodjunction, 'Type_ID')->hiddenInput()->label(false); ?>

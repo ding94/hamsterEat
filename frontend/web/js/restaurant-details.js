ec11b7c2afb11a3c84936c36dc990e807be745c4
darrensoap <darrensoap@gmail.com>
2018-01-18 15:58:22 +0800

    restaurant details ui changes

--- frontend/web/js/restaurant-details.js
+++ frontend/web/js/restaurant-details.js
@@ -1,12 +1,16 @@
-$(window).scroll(function() {
-    var height = $(window).scrollTop();
+$(document).ready(function(){
+	$(window).scroll(function() {
+		scrNav();
 
-    if(height  > 200) {
-      $('#category-bar').addClass('fixed');
-    }
-    if (height < 250) {
-      $('#category-bar').removeClass('fixed');
-    }
+	    var height = $(window).scrollTop();
+
+	    if(height  > 200) {
+	      $('#category-bar').addClass('fixed');
+	    }
+	    if (height < 250) {
+	      $('#category-bar').removeClass('fixed');
+	    }
+	})
 });
 
 $('.scroll-link').on('click', function(event){
@@ -18,4 +22,19 @@ $('.scroll-link').on('click', function(event){
 function scrollToID(id, speed){
     var targetOffset = $(id).offset().top - $("#category-bar").outerHeight()*3;
     $('html,body').animate({scrollTop:targetOffset}, speed);
+}
+
+var link = $('#category-bar a.scroll-link');
+
+function scrNav() {
+    var sTop = $(window).scrollTop();
+    $('.foodtype').each(function() {
+      var id = $(this).attr('id'),
+          offset = $(this).offset().top - $("#category-bar").outerHeight()*3,
+          height = $(this).height();
+      if(sTop >= offset && sTop < offset + height) {
+        link.removeClass('active-link');
+        $('#category-bar').find('[data-id="' + id + '"]').addClass('active-link');
+      }
+    });
 }

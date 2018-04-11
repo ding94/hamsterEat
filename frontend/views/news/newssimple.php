2b6bf53e46bf634259fd10db0b3020704bbebf14
darrensoap <darrensoap@gmail.com>
2018-04-11 16:12:58 +0800

    changes to t&c popup modal ui

--- frontend/views/news/newssimple.php
+++ frontend/views/news/newssimple.php
@@ -5,6 +5,7 @@ use frontend\assets\NewsAsset;
 
 NewsAsset::register($this);
 ?>
+<div class="content-background"></div>
 <div id="news-container" class="container">
 	<?php echo $news['text']; ?>
 </div>

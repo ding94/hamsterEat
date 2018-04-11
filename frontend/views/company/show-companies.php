22ba399194ded0361df2ad94e553c7d7de5be051
soonws <soonws97@gmail.com>
2018-04-11 15:39:42 +0800

    style create

--- frontend/views/company/show-companies.php
+++ frontend/views/company/show-companies.php
@@ -3,13 +3,13 @@ use yii\helpers\Html;
 use yii\helpers\Url;
 ?>
 <div class='row'>
-	<div class = "col-md-5">
+	<div class = "col-md-5" style='background-color: white;margin-left: 25%'>
 		<h2><b>Companies List</b></h2>
-		<table class='table table-hover'>
+		<table class='table table-hover col-md-6'>
 			<?php foreach ($list as $k => $company): ?>
 				<tr>
 					<td><?= $company['name'] ?></td>
-					<td><?= Html::a('<font style="color:blue">Register</font>',['/company/register-employee','cid'=>$company['id']], ['data-confirm'=>"Do you want to register as this company's employee?"]) ?></td>
+					<td><?= Html::a('<font">Register</font>',['/company/register-employee','cid'=>$company['id']], ['data-confirm'=>"Do you want to register as this company's employee?",'class'=>'btn btn-success']) ?></td>
 				</tr>
 			<?php endforeach; ?>
 		</table>

e6cd30b34db9acaa5f2be2c4938c437f7d8b02e8
soonws <soonws97@gmail.com>
2018-05-07 15:56:09 +0800

    delete back button

--- frontend/views/user/phone-detail.php
+++ frontend/views/user/phone-detail.php
@@ -37,7 +37,6 @@ UserAsset::register($this);
 
                 <div class="form-group">
                     <?= Html::submitButton(Yii::t('user','Update Profile'), ['class' => 'raised-btn main-btn change-password-resize-btn', 'name' => 'signup-button']) ?>
-                    <?php echo Html::a(Yii::t('common','Back'),['/user/user-profile'],['class'=>'raised-btn secondary-btn change-password-resize-btn'])?>
                 </div>
 
             <?php ActiveForm::end(); ?>
